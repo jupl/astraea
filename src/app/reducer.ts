75b94f9c69691e7f2a1fba121ea7d3e1867c633f
jupl <juan.placencia.512@gmail.com>
2016-10-21 23:10:43 -0500

    Remove dispatch in app reducer

--- src/app/reducer.ts
+++ src/app/reducer.ts
@@ -1,10 +1,7 @@
-import {combineReducers, Dispatch} from 'redux'
+import {combineReducers} from 'redux'
 
 /** Structure of application state */
-export type State = {} // Replace with Domain1 & Domain 2 & ...
-
-/** Dispatcher bound to application state */
-export type Dispatch = Dispatch<State>
+export type State = {} // Replace with Domain1 & Domain2 & ...
 
 /** Combination of reducer subsets as a single reducer for Redux */
 export default combineReducers<State>({})
