3efedd53ebb280fd091698a9046171d765bb1050
jupl <juan.placencia.512@gmail.com>
2017-01-28 02:03:01 -0600

    Remove container styling in storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -6,12 +6,6 @@ function loadStories() {
   // Load normalize.css
   require('normalize.css')
 
-  // Style root container
-  const {CONTAINER_STYLE} = require('../src/app/styles')
-  const root = document.getElementById('root')
-  root.style.cssText = ''
-  Object.assign(root.style, CONTAINER_STYLE)
-
   // Load all stories defined in our codebase
   const req = require.context('../src', true, /\.stories$/)
   req.keys().forEach(req)
