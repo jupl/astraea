dbf8fcdbe49cd5ee191608d4706572a748d99933
jupl <juan.placencia.512@gmail.com>
2017-01-28 13:42:35 -0600

    Use @types/webpack-env

--- src/common/declarations.d.ts
+++ src/common/declarations.d.ts
@@ -1,15 +1,3 @@
-/** Webpack hot module API */
-interface HotModuleAPI {
-  /** Accept code updates */
-  accept: Function
-}
-
-/** Extend module for possible addition of Webpack's hot module replacement */
-interface NodeModule {
-  /** Webpack's hot module replacement */
-  hot?: HotModuleAPI
-}
-
 interface Window {
   __REDUX_DEVTOOLS_EXTENSION__?: Function
 }
