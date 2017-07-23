4e5d772ab31ef77465ab21a6e484028aa00503e8
jupl <juan.placencia.512@gmail.com>
2017-07-22 19:01:16 -0500

    Fix storybook configuration

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,3 +1,3 @@
 // Reference our Webpack config for Storybook
 require('ts-node/register')
-module.exports = require('../webpack.config').default
+module.exports = require('../src/webpack/config/client').configuration
