90cf2a1c9487b6937e2195fd03ceebd19b4e3dd1
jupl <juan.placencia.512@gmail.com>
2017-10-04 21:09:23 -0500

    Update many things (including React 16)

--- setup-tests.ts
+++ setup-tests.ts
@@ -1,1 +1,15 @@
+import {configure} from 'enzyme'
+import * as Adapter from 'enzyme-adapter-react-16'
+import 'jest-enzyme'
+
+// Set up Enzyme
+configure({adapter: new (Adapter as any)()})
+
+// For React 16
+Object.assign(global, {
+  requestAnimationFrame(callback: Function) {
+    setTimeout(callback, 0)
+  },
+})
+
 // Add code to execute before tests are run
