2956e4aaf2883140e90cd5f0fea80ef1c3fe5eba
jupl <juan.placencia.512@gmail.com>
2017-10-24 21:43:40 -0500

    Cleanup

--- src/app/reducer.ts
+++ src/app/reducer.ts
@@ -4,5 +4,5 @@ import {createReducerCreator} from '../common/reducer'
 // tslint:disable-next-line:no-empty-interface
 export interface IState {} // Extend Domain1, Domain2, ...
 
-/** Reducr creator for potential extra reducers */
+/** Reducer creator for potential extra reducers */
 export const createReducer = createReducerCreator<IState>({})
