df675aba8934790c2b0d97e1c42e05966a8ebcb5
jupl <juan.placencia.512@gmail.com>
2017-10-29 12:59:39 -0500

    Remove lint skip

--- src/app/reducer.ts
+++ src/app/reducer.ts
@@ -1,7 +1,6 @@
 import * as Common from '../common/reducer'
 
 /** Structure of application state */
-// tslint:disable-next-line:interface-over-type-literal
 export type State = Common.State // = Domain1 & Domain2 & ...
 
 /** Reducer creator for potential extra reducers */
