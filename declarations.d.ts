f7e2b6ae0cde179723be5d2e60df329642f70f36
jupl <juan.placencia.512@gmail.com>
2018-04-06 13:58:57 -0500

    Fix more linting errors

--- declarations.d.ts
+++ declarations.d.ts
@@ -3,3 +3,19 @@
 interface Window {
   __REDUX_DEVTOOLS_EXTENSION__?: Function
 }
+
+// TODO Remove the following below once TS supports async iterator w/o esnext
+
+interface SymbolConstructor {
+  readonly asyncIterator: symbol
+}
+
+interface AsyncIterator<T> {
+  next(value?: {}): Promise<IteratorResult<T>>
+  return?(value?: {}): Promise<IteratorResult<T>>
+  throw?(e?: {}): Promise<IteratorResult<T>>
+}
+
+interface AsyncIterable<T> {
+  [Symbol.asyncIterator](): AsyncIterator<T>
+}
