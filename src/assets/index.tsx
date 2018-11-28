bbf8c0bd809f1c522c3e0961962aecdc949604f5
jupl <juan.placencia.512@gmail.com>
2018-11-27 19:20:09 -0600

    Simplify hot reload aspect

--- src/assets/index.tsx
+++ src/assets/index.tsx
@@ -3,26 +3,16 @@ import {render as renderToDOM} from 'react-dom'
 import {AppRoot} from '../app/components/root'
 import {Container} from '../common/components/container'
 
-// Reference app container to render to
+// Prevent unnecessary module refresh
+if(module.hot) {
+  module.hot.accept()
+}
+
+// Render application
 const ID = 'root'
 let container = document.getElementById(ID)
 if(!container) {
   container = Object.assign(document.createElement('div'), {id: ID})
   document.body.appendChild(container)
 }
-
-// Render application. Also register to rerender if hot loading is available.
-if(module.hot) {
-  module.hot.accept('../app/components/root', render)
-  module.hot.accept('../common/components/container', render)
-}
-render()
-
-/**
- * Render application to the container. If we are not in production and an
- * error is encountered the error is rendered to the screen. This may be called
- * multiple times to rerender when a hot reload occurs.
- */
-function render() {
-  renderToDOM(<Container><AppRoot /></Container>, container)
-}
+renderToDOM(<Container><AppRoot /></Container>, container)
