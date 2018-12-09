61b84a932003e2e04724da233191e6bad2899bcc
jupl <juan.placencia.512@gmail.com>
2018-12-09 08:45:14 -0600

    Update wcb config

--- webpack.config.ts
+++ webpack.config.ts
@@ -2,29 +2,26 @@ import {addRules, createConfiguration} from 'wcb'
 
 const isStorybook = process.env.STORYBOOK === 'true'
 
-let configuration = addRules(createConfiguration({
+// tslint:disable-next-line:no-default-export
+export default addRules(createConfiguration({
   assets: !isStorybook,
   atlOptions: {
     useBabel: true,
+  },
+  base: {
+    devServer: {
+      historyApiFallback: true,
+    },
+    output: {
+      publicPath: '/',
+    },
   },
   cssLoaders: [{test: /\.css$/, use: ['css-loader']}],
   destination: 'dist/assets',
   devServer: true,
   html: !isStorybook,
-  publicPath: '/',
   source: 'src/assets',
   split: true,
 }), [
   {test: /\.(gif|jpg|jpeg|png|svg)$/, use: ['file-loader']},
 ])
-
-configuration = {
-  ...configuration,
-  devServer: {
-    ...configuration.devServer,
-    historyApiFallback: true,
-  },
-}
-
-// tslint:disable-next-line:no-default-export
-export default configuration
