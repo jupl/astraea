e8f644b39d6ac39a66b09595784b4b14ca9aa29e
jupl <juan.placencia.512@gmail.com>
2019-04-23 14:52:04 -0500

    Update dependencies

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,4 +1,5 @@
 // Patch to fix tsconfig-paths plugin
+// tslint:disable-next-line:no-object-mutation
 Object.assign(process.env, {TS_NODE_PROJECT: ''})
 
 export {configuration as default} from './webpack.config.main'
